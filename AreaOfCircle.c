f95b3edac0bc88763ae64355cb8d46f0e23e315d
Aman Singh <ak0485125@gmail.com>
2025-04-13 14:43:44 +0530

    Add files via upload

---
+++ AreaOfCircle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int r = 143;
+    float pie = 3.1415;
+    float area = pie * r * r;
+    printf(" THE AREA OF CIRCLE IS : %f",area);
+
+    
+    return 0;
+}
+    
