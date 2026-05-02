4518c0ed97b9d8674743c4fa59269d01160543f3
NovaStream Dev <novastream@dev.local>
2026-05-02 16:12:45 +0530

    fix: vite build, useStore, index.css

--- client/src/store/useStore.ts
+++ client/src/store/useStore.ts
@@ -1,15 +1,12 @@
-﻿import { create } from 'zustand'
-
+import { create } from 'zustand'
 interface StoreState {
   user: any | null
   setUser: (user: any) => void
   clearUser: () => void
 }
-
 const useStore = create<StoreState>((set) => ({
   user: null,
   setUser: (user) => set({ user }),
   clearUser: () => set({ user: null }),
 }))
-
 export default useStore
